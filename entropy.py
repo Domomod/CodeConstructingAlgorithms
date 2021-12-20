3ee1eda66eafd11746f73a694c657f943b5ff166
Dominik Witczak <domionato@gmail.com>
2021-12-20 01:34:58 +0100

    Update entropy.py

--- entropy.py
+++ entropy.py
@@ -1,22 +1,12 @@
 import math
 from collections import Counter
 
-def eta(data):
+def eta(probs):
     base = 2
-
-    if len(data) <= 1:
-        return 0
-
-    counts = Counter()
-
-    for d in data:
-        counts[d] += 1
-
+    
     ent = 0
-
-    probs = [float(c) / len(data) for c in counts.values()]
     for p in probs:
         if p > 0.:
-            ent -= p * math.log(p, base)
+            ent -= float(p) * math.log(p, base)
 
     return ent
